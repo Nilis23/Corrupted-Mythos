444d9b1bc85298dd3f69b088436beaae75634af2
jpvogel07 <Jacob.Vogel@trojans.dsu.edu>
2021-08-26 13:39:41 -0500

    v.004
    attack animation will play, added base for main menu, added jump (still needs physics adjusting)

--- Corrupted Mythos/Assets/Scripts/Movement.cs
+++ Corrupted Mythos/Assets/Scripts/Movement.cs
@@ -5,14 +5,20 @@ using UnityEngine.InputSystem;
 
 public class Movement : MonoBehaviour
 {
+    public Animator atk;
     public CharacterController cPlayer;
     private Inputs pcontroller;
     private Vector2 desiredDirection;
+    private Rigidbody2D playerbody;
+    private BoxCollider2D playerbox;
+    private float jumpVelocity = 10f;
     [SerializeField]
     private int speed = 1;
 
     private void OnEnable()
     {
+        playerbox = transform.GetComponent<BoxCollider2D>();
+        playerbody = transform.GetComponent<Rigidbody2D>();
         cPlayer = GetComponent<CharacterController>();
         pcontroller = new Inputs();
         pcontroller.Enable();
@@ -41,24 +47,42 @@ public class Movement : MonoBehaviour
         desiredDirection.x = pcontroller.player.movement.ReadValue<Vector2>().x;
         cPlayer.Move(desiredDirection * Time.deltaTime * speed);
 
-        /*
+        
         if(pcontroller.player.movement.triggered){ 
-            if (pcontroller.player.movement.ReadValue<Vector2>().y>0) //checks if up button is inputed
+            if (pcontroller.player.movement.ReadValue<Vector2>().y>0 && isGrounded()) //checks if up button is inputed
             {
                 //jump
+                playerbody.velocity = Vector2.up * jumpVelocity;
+                
             }
             else if(pcontroller.player.movement.ReadValue<Vector2>().y < 0) //checks if down button is inputed
             {
 
             }
         }
-        */
+        
 
     }
 
     void attack()
     {
         //run animation 
+        /*
+        if (check if "space is false")
+        {
+            atk.SetBool("space", true);
+        }else{do nothing}
+        */
         //if hit calc damage
+
+    }
+
+    private bool isGrounded()
+    {
+        RaycastHit2D raycastHit2D = Physics2D.BoxCast(playerbox.bounds.center, playerbox.bounds.size, 0f, Vector2.down, .1f);
+
+
+
+        return raycastHit2D.collider != null;
     }
 }
