5b7fe5858d3c4dea58605cb211a66f4dfecc3a37
Nilis23 <noah.osborn@trojans.dsu.edu>
2021-09-16 11:08:37 -0500

    v0.15.6
    Added enemy health to enemy. Small logical modifications made to save on CPU power and increase efficiency

--- Corrupted Mythos/Assets/Scripts/EnemyHealth.cs
+++ Corrupted Mythos/Assets/Scripts/EnemyHealth.cs
@@ -6,19 +6,27 @@ public class EnemyHealth : MonoBehaviour
 {
     [SerializeField]
     public int health;
-    public Transform enemy;
-    void Update()
-    {
+    //public Transform enemy;
+
+    //void Update()
+    //{
+        /** -- Rather than using CPU time every frame, this could instead be called when the enemy is damaged to see if the enemy should die
         if (health<=0)
         {
             enemy.gameObject.SetActive(false);
         }
-    }
+        **/
+    //}
 
     public void minusHealth(int damage)
     {
         health -= damage;
         Debug.Log(health);
+        if(health <= 0)
+        {
+            this.gameObject.SetActive(false);
+            //Destroy(this);
+        }
     }
     public void addHealth(int gain)
     {
