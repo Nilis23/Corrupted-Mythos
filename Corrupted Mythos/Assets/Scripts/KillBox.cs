5a241170b5f5fd10db1eda9c1293adc4bc6712e8
Nilis23 <noah.osborn@trojans.dsu.edu>
2021-09-22 15:10:01 -0500

    v1.005.3
    Tested using far off background for parallax effect. It does not work. Adjusted the way the killbox works.

---
+++ Corrupted Mythos/Assets/Scripts/KillBox.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KillBox : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            collision.gameObject.GetComponent<PlayerHealth>().minusHealth(100);
+        }
+    }
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            collision.gameObject.GetComponent<PlayerHealth>().minusHealth(100);
+        }
+    }
+}
