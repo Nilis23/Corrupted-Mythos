9e931783ea0372e53800fb80a4483f623a3271db
jpvogel07 <Jacob.Vogel@trojans.dsu.edu>
2021-09-29 08:29:45 -0500

    v.007.3
    flaming tile timer works

--- Corrupted Mythos/Assets/Scripts/FlamingTiles.cs
+++ Corrupted Mythos/Assets/Scripts/FlamingTiles.cs
@@ -6,15 +6,39 @@ public class FlamingTiles : MonoBehaviour
 {
 
     public int damage = 20;
+    private float time;
+    private bool flaming=false;
     public PlayerHealth script;
+
+    private void Update()
+    {
+        if (flaming)
+        {
+            time += Time.deltaTime;
+        }
+
+        if (time>=1f)
+        {
+            script.minusHealth(damage);
+            time = 0;
+        }
+    }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.tag == "Player")//<--could change to while loop(?)
+        if (collision.gameObject.tag == "Player")
         {
             script = collision.GetComponent<PlayerHealth>();
-            script.minusHealth(damage);
-            //need it to repeat based on time standing on tile
+            flaming = true;
+        } 
+    }
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            script = collision.gameObject.GetComponent<PlayerHealth>();
+            flaming = false;
         }
+        
     }
 }
