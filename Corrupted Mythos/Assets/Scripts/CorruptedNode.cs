5bd7b71fa4828e6a8c4a241363d304d29625d155
Nilis23 <noah.osborn@trojans.dsu.edu>
2021-10-04 13:16:03 -0500

    v1.010
    Began working on the arena mode

---
+++ Corrupted Mythos/Assets/Scripts/CorruptedNode.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CorruptedNode : MonoBehaviour
+{
+    [Tooltip("This is the list of enemies this node can spawn")]
+    [SerializeField]
+    List<GameObject> EnemyList = new List<GameObject>();
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
