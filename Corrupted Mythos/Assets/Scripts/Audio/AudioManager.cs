0b0fcdb2913037061357bc1aa1d43810f4300ba6
Nilis23 <noah.osborn@trojans.dsu.edu>
2021-10-07 13:07:18 -0500

    v1.015.2
    Added audio system and fog

---
+++ Corrupted Mythos/Assets/Scripts/Audio/AudioManager.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class AudioManager : MonoBehaviour
+{
+    public Sound[] sounds;
+    public static AudioManager instance;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        if(instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(this.gameObject);
+            return;
+        }
+        DontDestroyOnLoad(this.gameObject);
+
+        foreach(Sound s in sounds)
+        {
+            //change to child component
+            GameObject ns = new GameObject();
+            ns.transform.parent = this.gameObject.transform;
+            ns.name = s.name;
+            s.source = ns.AddComponent<AudioSource>();
+
+            s.source.clip = s.clip;
+            s.source.volume = s.volume;
+            s.source.pitch = s.pitch;
+            s.source.loop = s.loop;
+        }
+    }
+
+    public void PlaySound(string name)
+    {
+        Sound s = Array.Find(sounds, sound => sound.name == name);
+        if(s != null)
+            s.source.Play();
+    }
+}
