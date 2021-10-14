d04bc330578826f8337bbde2a9c33339751902dd
Nilis23 <noah.osborn@trojans.dsu.edu>
2021-10-14 16:48:07 -0500

    axe storage base

--- Corrupted Mythos/Assets/Scripts/Projectiles/AxeProjectileTest.cs
+++ Corrupted Mythos/Assets/Scripts/Projectiles/AxeProjectileTest.cs
@@ -16,55 +16,24 @@ public class AxeProjectileTest : MonoBehaviour
     [SerializeField]
     float xSpeed;
     [SerializeField]
-    float launchAngle;
+    float maxHeight;
     [SerializeField]
     float timeToPeak;
     [SerializeField]
     float rotSpeed;
 
-    float a;
     float dir = 0;
     float t = 0;
 
-    float xOriginal;
+    float tOffset;
     float yOriginal;
 
-    /*
-    // Start is called before the first frame update
     void Start()
     {
-        a = (xSpeed * Mathf.Tan(angle) ) / timeToPeak;
-
-        if(this.transform.localScale.x > 0)
-        {
-            dir = 1;
-        }
-        else
-        {
-            dir = -1;
-        }
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        lifetime -= Time.deltaTime;
-        if(lifetime <= 0)
-        {
-            Destroy(this.gameObject);
-        }
-
-        t += Time.deltaTime;
-        float nY = (this.transform.position.y) + ((timeToPeak - t) * a);
-        transform.Translate(new Vector2(Time.deltaTime * xSpeed * dir, nY));
-    }
-    */
-
-    void Start()
-    {
-        xOriginal = transform.position.x;
         yOriginal = transform.position.y;
-
+        tOffset = -2 - Mathf.Sqrt(Mathf.Pow(2, 2) - 4*(-1)*( -1 * Mathf.Pow(timeToPeak, 2) + maxHeight)) / (2 * -1);
+        timeToPeak += Mathf.Abs(tOffset);
+        Debug.Log(tOffset.ToString());
         if (this.transform.localScale.x > 0)
         {
             dir = 1;
@@ -77,11 +46,13 @@ public class AxeProjectileTest : MonoBehaviour
     }
     void FixedUpdate()
     {
-        t += Time.deltaTime;
+        t += Time.fixedDeltaTime;
 
-        float newX = xSpeed * t * dir;  //x where xOriginal is 0
-        transform.position = new Vector2(xOriginal + (newX), (-1 * Mathf.Tan(launchAngle * Mathf.Deg2Rad) * (newX * newX)) / (2 * timeToPeak) + (xSpeed * Mathf.Tan(launchAngle * Mathf.Deg2Rad) * (newX * dir)) + yOriginal);
-        transform.Rotate(new Vector3(0, 0, Time.deltaTime * (dir * -1) * rotSpeed));
+        float newY = (-1 * Mathf.Pow(t - timeToPeak, 2)) + maxHeight + yOriginal;
+        float newX = xSpeed * Time.fixedDeltaTime * dir;
+
+        transform.position = new Vector2(transform.position.x + newX, newY);
+        transform.Rotate(new Vector3(0, 0, Time.fixedDeltaTime * (dir * -1) * rotSpeed));
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
