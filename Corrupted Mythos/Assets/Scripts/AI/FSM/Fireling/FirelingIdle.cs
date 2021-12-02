680f7d7561b94f8fd9630abe6c03d5a90a5930ab
Noah <noah.osborn@trojans.dsu.edu>
2021-12-02 14:00:18 -0600

    v2.031.2
    Idle state for fireling

---
+++ Corrupted Mythos/Assets/Scripts/AI/FSM/Fireling/FirelingIdle.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Idle State", menuName = "FSM/States/Fireling/Idle", order = 1)]
+public class FirelingIdle : State
+{
+    public State FirelingActive;
+
+    public override State RunCurrentState(StateManager em)
+    {
+        int colState = em.getCollisionState();
+        if (colState > 0) //If the collision state is greater than 0, transfer to the patrol state
+        {
+            em.idle = false;
+            return FirelingActive;
+        }
+
+        return null;
+    }
+
+    public override void StartState(StateManager em)
+    {
+        em.idle = true;
+    }
+
+}
