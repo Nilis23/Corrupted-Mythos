4a3c01de6b77258142bab217bfe4308cf6631cb6
jpvogel07 <Jacob.Vogel@trojans.dsu.edu>
2021-12-09 18:07:31 -0600

    v2.046.2
    minor changes (unimportant)

--- Corrupted Mythos/Assets/Scripts/Teleport.cs
+++ Corrupted Mythos/Assets/Scripts/Teleport.cs
@@ -5,14 +5,9 @@ using UnityEngine;
 public class Teleport : MonoBehaviour
 {
     public Animator Fading;
-    public GameObject location;
+    public Transform location;
 
     private PlayerMovement movement;
-
-    private void Start()
-    {
-        
-    }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
@@ -25,27 +20,18 @@ public class Teleport : MonoBehaviour
 
     IEnumerator teleport(GameObject player)
     {
-        /*
-        lock player falling
-        play fadeout
-        wait
-        take control away from player
-        teleport player
-        play fadein 
-        wait
-        give control back to player
-        */
-
         //player.GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.FreezePositionY;
-
+ 
         Fading.SetBool("teleporting", true);
         yield return new WaitForSeconds(1);
+
         player.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
         movement.paused = true;
-        player.transform.position = location.transform.position;
+        player.transform.position = location.position;
+
         Fading.SetBool("teleporting", false);
         yield return new WaitForSeconds(1);
+        
         movement.paused = false;
     }
-
 }
