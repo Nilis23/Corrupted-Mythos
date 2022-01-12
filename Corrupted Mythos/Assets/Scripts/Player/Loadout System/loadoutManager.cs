ff26be2c4339ec1c9db3c69133de095708ca9f89
Nilis23 <noah.osborn@trojans.dsu.edu>
2022-01-12 14:45:44 -0600

    v3.006
    Implementing new UI features

--- Corrupted Mythos/Assets/Scripts/Player/Loadout System/loadoutManager.cs
+++ Corrupted Mythos/Assets/Scripts/Player/Loadout System/loadoutManager.cs
@@ -12,9 +12,11 @@ public class loadoutManager : MonoBehaviour
     Artifact three;
     [Space]
     [SerializeField]
-    ImageController axe;
+    ImageController art1;
     [SerializeField]
-    ImageController horn;
+    ImageController art2;
+    [SerializeField]
+    ImageController art3;
 
     private Inputs pcontroller;
     private float[] timers = new float[3];
@@ -22,6 +24,10 @@ public class loadoutManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        art1 = GameObject.Find("Artifact1Ico").GetComponent<ImageController>();
+        art2 = GameObject.Find("Artifact2Ico").GetComponent<ImageController>();
+        art3 = GameObject.Find("Artifact3Ico").GetComponent<ImageController>();
+
         pcontroller = new Inputs();
         pcontroller.Enable();
 
@@ -40,18 +46,19 @@ public class loadoutManager : MonoBehaviour
         {
             one.doAction(this.gameObject);
             timers[0] = one.getTimer();
-            axe.DeactivateImage();
+            art1.DeactivateImage();
         }
         if(pcontroller.player.ArtifactTwo.triggered && two != null && timers[1] <= 0)
         {
             two.doAction(this.gameObject);
             timers[1] = two.getTimer();
-            horn.DeactivateImage();
+            art2.DeactivateImage();
         }
         if (pcontroller.player.ArtifactThree.triggered && three != null && timers[2] <= 0)
         {
             three.doAction(this.gameObject);
             timers[2] = three.getTimer();
+            art3.DeactivateImage();
         }
 
         for(int i = 0; i < timers.Length; i++)
@@ -59,11 +66,15 @@ public class loadoutManager : MonoBehaviour
             timers[i] -= Time.deltaTime;
             if(i == 0 && timers[i] <= 0)
             {
-                axe.ActivateImage();
+                art1.ActivateImage();
             }
-            if(i == 1 && timers[i] <= 0 && horn != null)
+            if(i == 1 && timers[i] <= 0 && art2 != null)
             {
-                horn.ActivateImage();
+                art2.ActivateImage();
+            }
+            if (i == 1 && timers[i] <= 0 && art3 != null)
+            {
+                art3.ActivateImage();
             }
         }
     }
