7b1651dcf9b1dfaa04d61cb8da4e3004d8e08517
Nilis23 <noah.osborn@trojans.dsu.edu>
2022-02-16 11:56:31 -0600

    v4.023.1
    Added an additional check to the controls for the god bar. Should be perfectly suited to implementation within a brief time.

--- Corrupted Mythos/Assets/Scripts/UI/GodBarControl.cs
+++ Corrupted Mythos/Assets/Scripts/UI/GodBarControl.cs
@@ -19,30 +19,26 @@ public class GodBarControl : MonoBehaviour
         barstart = 1;
         Bar.rectTransform.sizeDelta = new Vector2(barstart, 33.081f);
     }
-
-    /*
-    // Update is called once per frame
-    void Update()
-    {
-        if (t >= 1)
-        {
-            barstart += 1;
-            Bar.rectTransform.sizeDelta = new Vector2(barstart, 33.081f);
-            t = 0;
-        }
-        t += Time.deltaTime;
-    }
-    */
     
     public void IncrementBar(float valAdd)
     {
         barstart += valAdd;
+        if(barstart > barfull)
+        {
+            barstart = barfull;
+        }
         Bar.rectTransform.sizeDelta = new Vector2(barstart, 33.081f);
     }
 
     public void ResetBar()
     {
-        barstart = 0;
+        barstart = 1;
         Bar.rectTransform.sizeDelta = new Vector2(barstart, 33.081f);
+    }
+
+    //This allows other scripts to query what size the full bar should be for calculating how much to fill it in each instance
+    public float GetFullSize()
+    {
+        return barfull;
     }
 }
