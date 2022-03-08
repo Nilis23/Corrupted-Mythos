0d9ac68b3c5f52b816ba43d5aa28b36ef89ffa1a
jpvogel07 <Jacob.Vogel@trojans.dsu.edu>
2022-03-08 11:46:28 -0600

    v4.033
    obstacle I frames implemented

--- Corrupted Mythos/Assets/Scripts/Object/Obstacles.cs
+++ Corrupted Mythos/Assets/Scripts/Object/Obstacles.cs
@@ -7,6 +7,7 @@ public class Obstacles : MonoBehaviour
     public int damage;
     private PlayerHealth script;
     public bool flip;
+    public GameObject OtherSideHit;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
@@ -15,6 +16,16 @@ public class Obstacles : MonoBehaviour
             script = collision.gameObject.GetComponent<PlayerHealth>();
             script.minusHealth(damage);
             script.knockAround(flip);
+            StartCoroutine(IFrames());
         }
+    }
+
+    IEnumerator IFrames()
+    {
+        OtherSideHit.GetComponent<PolygonCollider2D>().enabled = false;
+        this.GetComponent<PolygonCollider2D>().enabled = false;
+        yield return new WaitForSeconds(1);
+        OtherSideHit.GetComponent<PolygonCollider2D>().enabled = true;
+        this.GetComponent<PolygonCollider2D>().enabled = true;
     }
 }
