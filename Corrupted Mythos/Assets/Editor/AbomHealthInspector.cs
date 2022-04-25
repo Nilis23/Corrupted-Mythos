834d9d1f48fd7c158a5f1a79759b335469aaec64
Nilis23 <noah.osborn@trojans.dsu.edu>
2022-04-25 10:40:36 -0500

    7.030
    Added a new inspector for the abom health subclass

---
+++ Corrupted Mythos/Assets/Editor/AbomHealthInspector.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEngine.UIElements;
+
+[CustomEditor(typeof(OWAbomHealth))]
+public class AbomHealthInspector : Editor
+{
+    public VisualTreeAsset m_InspectorXML;
+    public override VisualElement CreateInspectorGUI()
+    {
+        VisualElement myInspector = new VisualElement();
+
+        VisualTreeAsset visualtree = AssetDatabase.LoadAssetAtPath<VisualTreeAsset>("Assets/Editor/statemanager_rough");
+        m_InspectorXML.CloneTree(myInspector);
+
+        //VisualElement inspectorFoldout = myInspector.Q("Default_Inspector");
+        //InspectorElement.FillDefaultInspector(inspectorFoldout, serializedObject, this);
+
+        return myInspector;
+    }
+}
